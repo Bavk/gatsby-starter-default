bdbd4e6ac0646a2253c11c68cef2e213e3d2a959
Bavk <denmurdok@gmail.com>
2021-03-16 06:25:30 +0200

    Create pages.js

---
+++ src/pages/pages.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const pages = () => (
+  <Layout>
+    <SEO title="404: Not found" />
+    Test items
+  </Layout>
+)
+
+export default pages
